7b8a4befc67b1acc42ae7a11c4db6e0616efe1b1
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-22 20:10:26 +0530

    fetch blogs from Hashnode Api

--- thanks-template/pages/blogs.tsx
+++ thanks-template/pages/blogs.tsx
@@ -1,7 +1,45 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
-const blogs = () => {
+export type Query = {
+  query: string;
+};
+
+const Blogs = () => {
+  const [articles, setArticle] = useState();
+
+  async function gql(query: string, variables = {}) {
+    const data = await fetch("https://api.hashnode.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+
+    return data.json();
+  }
+
+  const GET_ARTICLES = `
+            query GetUserArticles($page: Int!) {
+                user(username: "sayuk") {
+                    publication {
+                        posts(page: $page) {
+                            title
+                            brief
+                            slug
+                        }
+                    }
+                }
+            }
+        `;
+
+  gql(GET_ARTICLES, { page: 0 }).then((result) => {
+    console.log(result.data.user.publication.posts);
+  });
   return (
     <div>
       <h1>Blogs</h1>
@@ -12,4 +50,4 @@ const blogs = () => {
   );
 };
 
-export default blogs;
+export default Blogs;
